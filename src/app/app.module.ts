49b62678e39b82377c53daa26d8e569f9ec730ec
TABET Gaël <gael.b.tabet@gmail.com>
2024-05-19 22:53:24 +0200

    afficher des users à partir d'un json ( api de teste)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DisplayContentComponent } from './display-content/display-content.component';
+import {HttpClientModule} from "@angular/common/http";
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DisplayContentComponent
+    DisplayContentComponent,
+    UserListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule // Ajouter HttpClientModule ici
   ],
   providers: [],
   bootstrap: [AppComponent]
