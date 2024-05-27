1c16d1543222a9cf4b59ef9cece8122b12701d29
TABET Gaël <gael.b.tabet@gmail.com>
2024-05-27 11:31:52 +0200

    Observable+install rxjs
    install RXJS
    Exercice sur Observable & observer

---
+++ src/app/rxjs-example/Observables&Observateurs.js
@@ -0,0 +1,20 @@
+// Importer Observable depuis RxJS
+const { Observable } = require('rxjs');
+
+// Créer un Observable de base
+const observable = new Observable(subscriber => {
+  subscriber.next('Hello');
+  subscriber.next('World');
+  subscriber.complete();
+});
+
+// Définir un Observer
+const observer = {
+  next: x => console.log('Observer got a next value: ' + x),
+  error: err => console.error('Observer got an error: ' + err),
+  complete: () => console.log('Observer got a complete notification')
+};
+
+// S'abonner à l'Observable avec l'Observer
+observable.subscribe(observer);
+
