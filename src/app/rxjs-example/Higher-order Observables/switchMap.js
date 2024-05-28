ed2c4caab4e45d23ced7b9f5a33d8d04a850e8be
TABET Gaël <gael.b.tabet@gmail.com>
2024-05-28 15:36:43 +0200

    Exercice SwitchMap

---
+++ src/app/rxjs-example/Higher-order Observables/switchMap.js
@@ -0,0 +1,14 @@
+//switchMap Annule les Observables précédents et n'émet que le dernier Observable.
+
+const { of, interval } = require('rxjs');
+const { switchMap, take } = require('rxjs/operators');
+
+interval(1000).pipe(
+  take(3),
+  switchMap(val => of(`Switch to ${val}`))
+).subscribe(console.log);
+
+// Output:
+// Switch to 0
+// Switch to 1
+// Switch to 2
